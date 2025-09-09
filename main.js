663dcc54a79376bebf90536bfcaf36dd974d128e
Hay1tov <eldorh35@gmail.com>
2025-09-09 14:23:26 +0500

    update

--- main.js
+++ main.js
@@ -48,80 +48,190 @@
 //                                                           No_2
 
 
-class Employee {
-  #id;
-  #firstName;
-  #lastName;
-  #salary;
+// class Employee {
+//   #id;
+//   #firstName;
+//   #lastName;
+//   #salary;
 
-  constructor(id, firstName, lastName, salary) {
-    this.#id = id;
-    this.#firstName = firstName;
-    this.#lastName = lastName;
-    this.#salary = salary;
-  }
+//   constructor(id, firstName, lastName, salary) {
+//     this.#id = id;
+//     this.#firstName = firstName;
+//     this.#lastName = lastName;
+//     this.#salary = salary;
+//   }
 
-  getId() {
-    return this.#id;
-  }
+//   getId() {
+//     return this.#id;
+//   }
 
-  getFirstName() {
-    return this.#firstName;
-  }
+//   getFirstName() {
+//     return this.#firstName;
+//   }
 
-  getLastName() {
-    return this.#lastName;
-  }
+//   getLastName() {
+//     return this.#lastName;
+//   }
 
-  getSalary() {
-    return this.#salary;
-  }
+//   getSalary() {
+//     return this.#salary;
+//   }
 
-  setId(id) {
-    this.#id = id;
-  }
+//   setId(id) {
+//     this.#id = id;
+//   }
 
-  setFirstName(fName) {
-    this.#firstName = fName;
-  }
+//   setFirstName(fName) {
+//     this.#firstName = fName;
+//   }
 
-  setLastName(lName) {
-    this.#lastName = lName;
-  }
+//   setLastName(lName) {
+//     this.#lastName = lName;
+//   }
 
-  setSalary(s) {
-    this.#salary = s;
-  }
+//   setSalary(s) {
+//     this.#salary = s;
+//   }
 
-  getFullName() {
-    return `${this.#firstName} ${this.#lastName}`;
-  }
+//   getFullName() {
+//     return `${this.#firstName} ${this.#lastName}`;
+//   }
 
-  getAnnualSalary() {
-    return this.#salary * 12;
-  }
+//   getAnnualSalary() {
+//     return this.#salary * 12;
+//   }
 
-  raiseSalary(percent) {
-    this.#salary += (this.#salary * percent) / 100;
-    return this.#salary;
-  }
-}
+//   raiseSalary(percent) {
+//     this.#salary += (this.#salary * percent) / 100;
+//     return this.#salary;
+//   }
+// }
 
-const emp = new Employee(1, "Ali", "Karimov", 500);
+// const emp = new Employee(1, "Ali", "Karimov", 500);
 
-console.log(emp.getId());
-console.log(emp.getFullName());
-console.log(emp.getAnnualSalary());
+// console.log(emp.getId());
+// console.log(emp.getFullName());
+// console.log(emp.getAnnualSalary());
 
-emp.raiseSalary(10);
-console.log(emp.getSalary());
+// emp.raiseSalary(10);
+// console.log(emp.getSalary());
 
 
+//                    Darsda ishlandi                        No_3
+//                    Darsda ishlandi                        No_4
 
-//                                                           No_3
-//                                                           No_4
 //                                                           No_5
+
+
+// class Shape {
+//   #color;
+//   #filled;
+
+//   constructor(color = "red", filled = true) {
+//     this.#color = color;
+//     this.#filled = filled;
+//   }
+
+//   getColor() {
+//     return this.#color;
+//   }
+
+//   setColor(color) {
+//     this.#color = color;
+//   }
+
+//   isFilled() {
+//     return this.#filled;
+//   }
+
+//   setFilled(filled) {
+//     this.#filled = filled;
+//   }
+// }
+
+// class Circle extends Shape {
+//   #radius;
+
+//   constructor(radius = 1.0, color = "red", filled = true) {
+//     super(color, filled);
+//     this.#radius = radius;
+//   }
+
+//   getRadius() {
+//     return this.#radius;
+//   }
+
+//   setRadius(radius) {
+//     this.#radius = radius;
+//   }
+
+//   getArea() {
+//     return Math.PI * this.#radius * this.#radius;
+//   }
+
+//   getPerimeter() {
+//     return 2 * Math.PI * this.#radius;
+//   }
+// }
+
+// class Rectangle extends Shape {
+//   #width;
+//   #height;
+
+//   constructor(width = 1.0, height = 1.0, color = "red", filled = true) {
+//     super(color, filled);
+//     this.#width = width;
+//     this.#height = height;
+//   }
+
+//   getWidth() {
+//     return this.#width;
+//   }
+
+//   setWidth(width) {
+//     this.#width = width;
+//   }
+
+//   getHeight() {
+//     return this.#height;
+//   }
+
+//   setHeight(height) {
+//     this.#height = height;
+//   }
+
+//   getArea() {
+//     return this.#width * this.#height;
+//   }
+
+//   getPerimeter() {
+//     return 2 * (this.#width + this.#height);
+//   }
+// }
+
+// const shape = new Shape("blue", false);
+// console.log("Shape color:", shape.getColor());
+// // console.log("Shape filled:", shape.isFilled());
+
+// const circle = new Circle(5, "green", true);
+// console.log("\nCircle color:", circle.getColor());
+// // console.log("Circle filled:", circle.isFilled());
+// // console.log("Circle area:", circle.getArea().toFixed(3)); 
+// // console.log("Circle perimeter:", circle.getPerimeter().toFixed(3));
+
+// const rect = new Rectangle(4, 6, "yellow", true);
+// console.log("\nRectangle color:", rect.getColor());
+// // console.log("Rectangle area:", rect.getArea());
+// // console.log("Rectangle perimeter:", rect.getPerimeter());
+
+
+
 //                                                           No_6
+
+
+
+
+
 //                                                           No_7
 //                                                           No_8
 //                                                           No_9
