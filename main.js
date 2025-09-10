b5e65e6402a0f6997240ce446eb61e80acf4f1ab
Hay1tov <eldorh35@gmail.com>
2025-09-10 16:14:19 +0500

    update

--- main.js
+++ main.js
@@ -349,9 +349,197 @@
 //                                                           No_7
 
 
+// class Animal {
+//   constructor(name, speed, weight) {
+//     this.name = name;
+//     this.speed = speed;
+//     this.weight = weight;
+//   }
+// }
 
+// class Mammal extends Animal {
+//   #legs;
+
+//   constructor(name, speed, weight, legs) {
+//     super(name, speed, weight);
+//     this.#legs = legs;
+//   }
+
+//   getLegs() {
+//     return this.#legs;
+//   }
+
+//   setLegs(legs) {
+//     if (legs >= 0) {
+//       this.#legs = legs;
+//     } else {
+//       console.log("( legs ) lar soni musbat bo'lishi kerak!");
+//     }
+//   }
+// }
+// class Cat extends Mammal {
+//   constructor(name, speed, weight, legs) {
+//     super(name, speed, weight, legs);
+//   }
+
+//   getSound() {
+//     return "Meow";
+//   }
+// }
+// class Dog extends Mammal {
+//   constructor(name, speed, weight, legs) {
+//     super(name, speed, weight, legs);
+//   }
+
+//   getSound() {
+//     return "Woof";
+//   }
+// }
+// class Fish extends Animal {
+//   #size;
+
+//   constructor(name, speed, weight, size) {
+//     super(name, speed, weight);
+//     this.#size = size;
+//   }
+
+//   getSize() {
+//     return this.#size;
+//   }
+
+//   setSize(size) {
+//     if (size > 0) {
+//       this.#size = size;
+//     } else {
+//       console.log(" ( size ) musbat bo'lishi kerak!");
+//     }
+//   }
+// }
+// class Whale extends Fish {
+//   constructor(name, speed, weight, size) {
+//     super(name, speed, weight, size);
+//   }
+
+//   getSound() {
+//     return "Poof";
+//   }
+// }
+// class Shark extends Fish {
+//   constructor(name, speed, weight, size) {
+//     super(name, speed, weight, size);
+//   }
+
+//   getSound() {
+//     return "Sheef";
+//   }
+// }
+
+// const cat = new Cat("Kitty", 20, 5, 4);
+// console.log("Cat:");
+// console.log(cat.name);
+// console.log(cat.getLegs());
+// console.log(cat.getSound());
+
+// const dog = new Dog("Rex", 30, 10, 4);
+// console.log("\nDog:");
+// console.log(dog.name);
+// console.log(dog.getLegs());
+// console.log(dog.getSound());
+
+// const whale = new Whale("Blue Whale", 15, 30000, 25);
+// console.log("\nWhale:");
+// console.log(whale.name);
+// console.log(whale.getSize());
+// console.log(whale.getSound());
+
+// const shark = new Shark("Great White", 40, 2000, 12);
+// console.log("\nShark:");
+// console.log(shark.name);
+// console.log(shark.getSize());
+// console.log(shark.getSound());
 
 
 //                                                           No_8
-//                                                           No_9
-//                                                           No_10
+
+
+// class CustomObject {
+//   static customKeys(obj) {
+//     return Object.keys(obj);
+//   }
+
+//   static customValues(obj) {
+//     return Object.values(obj);
+//   }
+
+//   static customEntries(obj) {
+//     return Object.entries(obj);
+//   }
+// }
+
+// class CustomNumber {
+//   static customIsInteger(num) {
+//     return Number.isInteger(num);
+//   }
+// }
+
+// class CustomArray {
+//   static customIsArray(arr) {
+//     return Array.isArray(arr);
+//   }
+// }
+
+// const person = { name: "Ali", age: 20 };
+
+// console.log(CustomObject.customKeys(person));
+// console.log(CustomObject.customValues(person));
+// console.log(CustomObject.customEntries(person));
+
+// console.log(CustomNumber.customIsInteger(10));
+// console.log(CustomNumber.customIsInteger(10.5));
+
+// console.log(CustomArray.customIsArray([1, 2, 3]));
+// console.log(CustomArray.customIsArray(12345));
+
+
+//                Pass                                       No_9
+
+//                    10 ni yarmi ChatGPT                    No_10
+
+class Person {
+  constructor(housePrice, housesNumber, carPrice, carsNumber, bankAccount) {
+    this.housePrice = housePrice;
+    this.housesNumber = housesNumber;
+    this.carPrice = carPrice;
+    this.carsNumber = carsNumber;
+    this.bankAccount = bankAccount;
+  }
+
+  getWealth() {
+    return (this.housePrice * this.housesNumber) +
+      (this.carPrice * this.carsNumber) +
+      this.bankAccount;
+  }
+}
+
+class RichPerson extends Person {
+  constructor(housePrice, housesNumber, carPrice, carsNumber, bankAccount,
+    companyPrice, companiesNumber, investigation) {
+    super(housePrice, housesNumber, carPrice, carsNumber, bankAccount);
+    this.companyPrice = companyPrice;
+    this.companiesNumber = companiesNumber;
+    this.investigation = investigation;
+  }
+
+  getWealth() {
+    const inheritedWealth = super.getWealth();
+    return inheritedWealth +
+      (this.companyPrice * this.companiesNumber) +
+      this.investigation;
+  }
+}
+
+const ali = new Person(50000, 2, 20000, 1, 10000);
+console.log("Ali boyligi:", ali.getWealth());
+
+const vali = new RichPerson(100000, 3, 30000, 2, 50000, 200000, 2, 1000000);
+console.log("Vali boyligi:", vali.getWealth());
